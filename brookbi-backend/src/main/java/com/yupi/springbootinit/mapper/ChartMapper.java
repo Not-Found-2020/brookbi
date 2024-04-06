99ac367f624c96f305ac14b371e519f5c6b2fefc
dreambrook <notfound@123.com>
2024-04-06 18:59:20 +0800

    first commit

---
+++ brookbi-backend/src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,14 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author lenovo
+* @description 针对表【chart(图表信息表)】的数据库操作Mapper
+* @createDate 2024-03-14 20:03:19
+* @Entity com.yupi.springbootinit.model.entity.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
